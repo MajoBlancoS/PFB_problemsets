27a46c060b0bf437acd64e6262be61df7b8a4163
pfb06 <pfb06@info06.cshl.edu>
2023-10-18 20:36:52 -0400

    fourth problem set, script4-range, Oct17

---
+++ Day3/fourth_problem_set_lists_loops_4.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+# Answer to Python problem set 4 - loops and lists
+# Write a script that uses range() in for loop to print out every value between 1 and 100
+for num in range(1,100):
+	print(num)
